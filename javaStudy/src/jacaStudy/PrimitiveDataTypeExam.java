1e292cdd79e45e82eb6c8e3bf8e8546dfaab6be4
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-20 15:25:48 +0900

    java study

---
+++ javaStudy/src/jacaStudy/PrimitiveDataTypeExam.java
@@ -0,0 +1,15 @@
+package jacaStudy;
+
+public class PrimitiveDataTypeExam {
+public static void main(String[] args) {
+	
+	boolean isFun = false;
+	System.out.println(isFun);
+	
+	char c = 'f';
+	int x = 59;
+	long bing = 3434445555l;
+	float f = 32.4f;
+	double d = 343434433.5;
+}
+}
---
+++ javaStudy/src/jacaStudy/VariableExam.java
@@ -0,0 +1,19 @@
+package jacaStudy;
+
+public class VariableExam {
+
+	public static void main(String[] args) {
+		
+		int count;
+		count=10;
+		count=20;
+		System.out.println(count);
+		
+		double avg = 11.1;
+		
+		String name="Jerry";
+		
+		int totalCount;
+		
+	}
+}
